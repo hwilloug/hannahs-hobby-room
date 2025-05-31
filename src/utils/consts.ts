c452f6740339b6458efa0b2e5ed5df3941881957
Hannah Willoughby <hannah@tcah.com>
2025-05-30 20:16:38 -0400

    Use env var for api url

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -1,7 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = "Hannah's Hobby Room";
 export const SITE_DESCRIPTION = 'A blog by Hannah Willoughby. Explore my hobbies and maybe find something new to try!';
 
-export const API_URL = 'https://blog-api.poppyland.dev';
+export const API_URL = import.meta.env.PUBLIC_API_URL || 'https://blog-api.poppyland.dev';
